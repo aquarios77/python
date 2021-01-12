072e5cf7574f12934cb58b422e41a6bec6ba5db3
aquarios77 <antons.sincovs@office.balticcoal.com>
2021-01-12 20:53:56 +0200

    6 corrected 5 area added

--- 2021-01-12/lekcija#03-uzd5.py
+++ 2021-01-12/lekcija#03-uzd5.py
@@ -2,5 +2,8 @@ a = int(input("Enter first side: "))
 b = int(input("Enter second side: "))
 c = int(input("Enter third side: "))
 
+p = (a+b+c)/2
+s = (p(p-a)(p-b)(p-c))**0.5
+
 if( (a+b)>c and (a+c)>b and (b+c)>a): print("Triangle exists!")
-else: print("No triangle!")3
+else: print("No triangle!")
--- 2021-01-12/lekcija#03-uzd6.py
+++ 2021-01-12/lekcija#03-uzd6.py
@@ -4,9 +4,12 @@ c = int(input("Enter c: "))
 
 d = (b**2 - 4*a*c)
 
-if(d>0):
+if(d>=0):
     x1 = (-b + d**0.5)/2*a
     x2 = (-b - d**0.5)/2*a
-    print("x1=",x1," x2=",x2)
+    if(x1!=x2):
+        print("x1=",x1," x2=",x2)
+    else:
+        print("x=",x1)
 else:
     print("D is negative!")
