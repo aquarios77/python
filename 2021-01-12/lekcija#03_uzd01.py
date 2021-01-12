8ef742be9491e22faf64fba1a4f349d26c79f821
aquarios77 <antons.sincovs@office.balticcoal.com>
2021-01-12 19:52:23 +0200

    rename and l3u1

---
+++ 2021-01-12/lekcija#03_uzd01.py
@@ -0,0 +1,10 @@
+x1 = 2
+x2 = 6
+y1 = 1
+y2 = 3
+
+x_test = int(input("Enter x: "))
+y_test = int(input("Enter y: "))
+
+if( 2<=x_test<=6 and 1<=y_test<=3): print("Punkts atrodas uz plaknes")
+else: print("Punkts ir arpus plaknes")
